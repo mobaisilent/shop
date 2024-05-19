7435f93ceeb2468431e4e7f8f56d078b77381ae4
mobai <mobaisilent@gmail.com>
2024-05-19 17:13:47 +0800

    add API to login with 70%

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,11 +1,5 @@
 <script setup>
-// 添加回车处理事件：后js判断输入是否正确
-function handleKeyDown(event) {
-  if (event.key === "Enter") {
-    console.log("回车键已按下！");
-  }
-}
-
+import { ref } from "vue";
 // 为忘记密码和添加用户两个span添加点击信息
 // windows.onload可以实现确保页面加载完毕之后执行后续代码段
 window.onload = function () {
@@ -27,9 +21,53 @@ window.onload = function () {
   }
   // ok 两个都跳转成功
 };
+// 上面是 忘记密码和注册用户的跳转
 
 
-// 添加跳转事件：实现点击后就行跳转到对应的检测页面
+
+// 下面是输入的逻辑判断和对应的信息提示 用@submit进行绑定
+const usernameRef = ref('');
+const passwordRef = ref('');  // 创建响应式变量
+function handleSubmit(event) {
+  event.preventDefault();
+  if (!usernameRef.value) {
+    alert("请输入用户名");
+  } else if (!passwordRef.value) {
+    alert("请输入密码");
+  } else {
+    fetch('http://localhost:4000/api/v1/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        userName: usernameRef.value,
+        password: passwordRef.value
+      })
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        alert("登录成功,点击确定跳转");
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+        alert("登录失败，请检查你的用户名和密码");
+      });
+  }
+}
+
+
+// 和上面类似就是为 form添加一个事件，不过上面的跳转时为下面 忘记密码 和 注册用户 添加点击事件
+
+
+
+// 下面还差利用 api  进行交互了
 </script>
 
 <template>
@@ -42,15 +80,16 @@ window.onload = function () {
       <span>请输入登录账户和密码</span>
     </div>
 
-    <form class="login-form" action="/html/index/index.html" method="post" novalidate>
+    <form class="login-form" action="/html/index/index.html" method="post" novalidate @submit="handleSubmit">
       <!--输入框-->
       <div class="input-content">
         <!--autoFocus-->
         <div>
-          <input type="text" autocomplete="off" placeholder="用户名" name="username" required />
+          <input type="text" v-model="usernameRef" autocomplete="off" placeholder="用户名" name="username" required />
         </div>
         <div style="margin-top: 16px">
-          <input type="password" autocomplete="off" placeholder="登录密码" name="password" required maxlength="32" />
+          <input type="password" v-model="passwordRef" autocomplete="off" placeholder="登录密码" name="password" required
+            maxlength="32" />
         </div>
       </div>
 
@@ -68,6 +107,7 @@ window.onload = function () {
     </form>
   </div>
 </template>
+
 <!-- 创建表单输入框实现提交数据，创建登录页面 -->
 <!-- 已index为跳板（原始路径）启动vue项目 其他模块通过跳转实现吧 -->
 
