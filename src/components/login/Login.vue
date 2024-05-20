2354dbf68a1d4749a45b32189a9530389730461f
mobai <mobaisilent@gmail.com>
2024-05-20 18:05:55 +0800

    add 跳过登录 for login function

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -14,16 +14,28 @@ window.onload = function () {
   const element2 = document.getElementById("forgetPassword");
   if (element2) {
     element2.addEventListener('click', () => {
-      window.location.href = "../../html/login/forget.html";
+      window.location.href = "";
     });
   } else {
     console.error("Element not found");
   }
   // ok 两个都跳转成功
+  const temp = document.getElementById("temp");
+  temp.addEventListener("click", () => {
+    window.location.href = "../../html/index/index.html"
+  });
+
 };
 // 上面是 忘记密码和注册用户的跳转
 
+
+
+// 是否登录成功显示动画
 const jumpdiv = ref(false);
+
+
+//保存公共信息
+var storage = window.localStorage;
 
 // 下面是输入的逻辑判断和对应的信息提示 用@submit进行绑定
 const usernameRef = ref('');
@@ -61,15 +73,27 @@ function handleSubmit(event) {
         }
         else if (statu == 200 && msg == "登录成功") {
           console.log(data.data.token);
-          // alert("登录成功,点击确定跳转");
+          // token信息测验  
+          // alert("登录成功,点击 确定跳转");
           // 提示信息
+
+          // 下面是保存公共信息
+          var json = JSON.stringify(data);
+          // json = JSON.parse(json);
+          // console.log(typeof (json));
+          // console.log(json);
+          // 现在json是一个json类型的object
+          storage.setItem("json", json);
+          console.log(json);
+
+          // 下面是动态界面的判断逻辑
           document.getElementById("main").style.display = "none";
           jumpdiv.value = !jumpdiv.value;
           // what can i say :这里错了我好久
-          // setTimeout(function () {
-          //   window.location.href = "/html/index/index.html";
-          // }, 2000); // 2000毫秒后执行，即2秒后
-          // 先临时注释掉测试返回的token
+          setTimeout(function () {
+            window.location.href = "/html/index/index.html";
+          }, 2000); // 2000毫秒后执行，即2秒后
+          // 先临时注释掉测试返回的token  :修改完毕：页面正常跳转
         }
 
         else if (statu == 500 && msg == "用户名不存在") {
@@ -122,7 +146,7 @@ import "../../JS/login/loading_img.js"
 
       <div class="foor">
         <div class="left" id="forgetPassword"><span>忘记密码 ?</span></div>
-
+        <div class="temp" id="temp"><span>跳过登录</span></div>
         <div class="right" id="addUser"><span>注册账户</span></div>
       </div>
       <!-- 添加注册用户信息  -->
