8f92bd42bbf1805d769c96939420ef762b2b31fe
mobai <mobaisilent@gmail.com>
2024-05-22 10:43:26 +0800

    restore vite.conf.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'node:path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
-  server: {
-    proxy: {
-      "api/v1": {
-        target: "http://127.0.0.1/api/v1",
-        ws: true,
-        changeOrigin: true,
-        rewrite: (path) => path.replace("/api/v1","")
-      }
-    }
-  }
 });
