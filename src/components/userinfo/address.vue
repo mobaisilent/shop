cd83dd1777041e39cbbb68d89042870662a2ed6c
mobai <mobaisilent@gmail.com>
2024-05-23 17:12:19 +0800

    finish the function of address

--- src/components/userinfo/address.vue
+++ src/components/userinfo/address.vue
@@ -28,13 +28,15 @@ async function fetchAddress() {
     console.log(response);
     const data = await response.json();
     console.log(data);
-    if (data.address) {
-      address.value = data.address;
-    } else {
+    console.log(data.data.total);
+    if (data.data.total == 0) {
       alert('没有地址信息');
+    } else {
+      // 这里实现显示地址：：通过响应式变量实现
+
     }
   } catch (error) {
-    alert('获取地址信息失败');
+    alert('响应失败，请检查服务器/你的网络状态');
     console.error('There has been a problem with your fetch operation:', error);
   }
 };
@@ -64,10 +66,42 @@ async function deleteAddress() {
   }
 }
 
+// 写一个响应式tag实现创建地址：就不是用新的页面/组件了
+const creatbutton = ref(true);
 function changeaddress() {
-
+  creatbutton.value = false;
 }
+// 响应式显示创建成功
 
+const newaddress = ref('');// 先创建一个空的响应式变量
+// 向响应式变量中添加数据用于测试：多利用这些测试方法
+function addresform() {
+  // 处理表单事件的响应
+  event.preventDefault();
+  fetch("http://localhost:4000/api/v1/address", {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+    },
+    body: JSON.stringify({
+      Address: newaddress,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log(response);
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    }).then(data => {
+      console.log(data);
+      // 打印响应数据用于调试查看
+      alert("创建地址" + '\n' + "`"
+        + newaddress.value + "`" + '\n'
+        + "成功");
+    });
+    creatbutton.value = true;
+}
 </script>
 
 
@@ -81,11 +115,18 @@ function changeaddress() {
       <button id="nextpage" @click="changeaddress" v-show="judge">下一页</button>
       <!-- 地址显示模块 -->
     </div>
-    <div class="address-container">
+    <div class="address-container" v-show="creatbutton">
+      <button @click="changeaddress">创建地址</button>
       <button @click="fetchAddress">显示地址</button>
       <button @click="deleteAddress">删除地址</button>
     </div>
-    <div id="info">请谨慎展示/删除地址 </div>
+    <div class="create" v-show="!creatbutton">
+      <form action="" @submit="addresform">
+        新地址<input type="text" v-model="newaddress">
+        <br> <button style="margin-top: 10px;">确认创建</button>
+      </form>
+    </div>
+    <div id="info">请谨慎删除地址 </div>
   </div>
 </template>
 
@@ -148,6 +189,13 @@ body {
 }
 
 .address-container button:first-of-type {
+  background-color: pink;
+  color: black;
+  border-radius: 24px;
+}
+
+/* 对于不是 首尾的使用 nth实现 */
+.address-container button:nth-of-type(2) {
   background-color: #64ee66;
   color: black;
   border-radius: 24px;
@@ -157,8 +205,8 @@ body {
   background-color: #f44336;
   color: white;
   border-radius: 24px;
-
 }
+
 
 #info {
   position: absolute;
@@ -171,5 +219,21 @@ body {
   display: block;
   width: 100%;
   height: 40px;
+}
+
+.create {
+  width: 220px;
+  height: 100px;
+  margin-top: 10%;
+  margin-left: 32%;
+  padding-top: 20px;
+  background-color: #ffffff;
+}
+
+.create input {
+  margin-top: 8px;
+  border: 2px solid black;
+  width: 90%;
+  height: 20px;
 }
 </style>
