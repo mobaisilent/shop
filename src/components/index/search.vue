e10b9d68ea663dada1d30cc939a11f794a91994e
mobai <mobaisilent@gmail.com>
2024-05-25 12:02:53 +0800

    finish the func for search product

--- src/components/index/search.vue
+++ src/components/index/search.vue
@@ -9,14 +9,11 @@ const allproduct = JSON.parse(window.localStorage.getItem("allproduct"));
 // console.log(typeof (searchwhat));
 // console.log(searchwhat);
 // 信息调试
+const showinfo = ref('');
+const clickbutton = ref(false);
 const closeWindow = () => {
-  console.log("here is son's item");
-  // console.log(allproduct.data.total);
-  // console.log(typeof (allproduct));
-  console.log(allproduct.data.item[1]);
-  // 还是这种原生格式使用起来比较方便：现在就是信息检索
-  // 放在外面反而打印不出来：那么就关闭的时候打印一次就行
   ifsearch.value = false;
+  clickbutton.value = false;
 };
 // 实现关闭
 // 通过父级向子级传递信息，不用子级向外级传递信息了：：稍微更加复杂了
@@ -27,15 +24,53 @@ const closeWindow = () => {
 // 键盘应该推进去的，不然都影响打字了
 // 下面调用商品查询API接口：实现筛选想要的信息
 // 准确来说应该是点击搜索的瞬间调用API实现向下传递JSON数据
+
+
+function ifbutton() {
+  clickbutton.value = !clickbutton.value;
+  console.log("here is son's item");
+  // console.log(allproduct.data.total);
+  // console.log(typeof (allproduct));
+  // console.log(allproduct.data.item[1].name);
+  // 还是这种原生格式使用起来比较方便：现在就是信息检索
+  // 放在外面反而打印不出来：那么就关闭的时候打印一次就行
+  if (searchwhat.value.length == 0) {
+    alert("请输入搜索内容");
+  }
+  else {
+    var i;
+    for (i = 0; i < allproduct.data.item.length; i++) {
+      if (searchwhat.value == allproduct.data.item[i].name) {
+        console.log("here");
+        console.log("here i=" + i);
+        break;
+      }
+    }
+    showinfo.value = allproduct.data.item[i];
+    console.log(showinfo.value);
+  }
+}
 </script>
 
 <template>
   <div v-if="ifsearch" class="search">
-    <div class="showinfo">
-      {{ searchwhat }}
-      {{ allproduct }}
+    <button @click="ifbutton" class="newbutton">点击展开/关闭</button>
+    <div v-show="clickbutton">
+      <div class="showinfo" v-if="showinfo">
+        <h2>商品详情</h2>
+        <p><strong>ID:</strong> {{ showinfo.id }}</p>
+        <p><strong>名称:</strong> {{ showinfo.name }}</p>
+        <p><strong>分类ID:</strong> {{ showinfo.categoryID }}</p>
+        <p><strong>标题:</strong> {{ showinfo.title }}</p>
+        <p><strong>描述:</strong> {{ showinfo.info }}</p>
+        <p><strong>图片:</strong> <img :src="showinfo.imgPath" alt="Product Image" class="product-image" /></p>
+        <p><strong>价格:</strong> {{ showinfo.price }}</p>
+        <p><strong>折扣价:</strong> {{ showinfo.discountPrice }}</p>
+        <p><strong>是否打折:</strong> {{ showinfo.onSale ? '是' : '否' }}</p>
+        <p><strong>库存数量:</strong> {{ showinfo.num }}</p>
+      </div>
+      <button class="close" @click="closeWindow">关闭</button>
     </div>
-    <button class="close" @click="closeWindow">关闭</button>
   </div>
 </template>
 
@@ -46,7 +81,7 @@ const closeWindow = () => {
   left: 50%;
   transform: translateX(-50%);
   width: 500px;
-  height: 200px;
+  height: auto;
   background-color: white;
   z-index: 1000;
   padding: 20px;
@@ -72,10 +107,36 @@ button {
 
 .showinfo {
   margin-top: 15px;
-  height: 100%;
-  ;
+}
+
+.showinfo h2 {
+  margin-bottom: 15px;
+  color: #333;
+}
+
+.showinfo p {
+  margin: 5px 0;
+  font-size: 16px;
+  line-height: 1.5;
+}
+
+.product-image {
+  max-width: 100px;
+  max-height: 100px;
+  width: auto;
+  height: auto;
+  display: block;
+  margin-top: 5px;
+}
+
+.newbutton {
+  /* position: relative; */
+  /* display: block; */
+  left: 39%;
+  width: 120px;
+  /* top: 90%;; */
   /* background-color: pink; */
-  /* 用于查看定位信息 */
+  border: 2px black solid;
 }
 </style>
 <!-- 创建为悬浮窗的格式就实现了覆盖下面的图片 -->
